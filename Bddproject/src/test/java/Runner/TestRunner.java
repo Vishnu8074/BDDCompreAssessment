a3f00a23f53e8c428078d10ca80b570f80787b55
Vishnu8074 <sallepallevishnuvardhanreddy@gmail.com>
2021-11-22 18:22:42 +0530

    commit1

--- Bddproject/src/test/java/Runner/TestRunner.java
+++ Bddproject/src/test/java/Runner/TestRunner.java
@@ -1,15 +1,18 @@
 
 package Runner;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+//@RunWith(Cucumber.class)
 @CucumberOptions(features = "Features", glue = "StepDefinitions")
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
 
